7cb06bd507841c7c2b500c73cc0e66ce05db30e0
Pablo Úbeda <pubedac@campusdigitalfp.com>
2026-02-27 11:07:01 +0100

    Añadida estructura PHP de 'if, elseif' para selección de contenido en sercontratada.php

--- sercontratada.php
+++ sercontratada.php
@@ -13,21 +13,31 @@
     <p><b>Ser Contratada</b></p>
     <p>Escoge, del siguiente selector, el apartado que quieres leer:</p>
     <form action="" method='POST'>
-        <select class="opciones">
+        <select class="opciones" name="opciones">
             <option value="edad" name="edad">Edad</option>
             <option value="nacionalidad" name="nacionalidad">Nacionalidad</option>
             <option value="relaciones" name="relaciones">Relaciones Excluidas y Especiales</option>
         </select>
-        <div><input type="button" action="submit" value="Mostrar apartado"></div>
+        <div><button type="Submit" value="Submit">Mostrar apartado</button></div>
     </form>
 </section>
 
 <section class="subapartados">
+
 <?php 
 if (isset($_POST['opciones'])){
     $opciones=$_POST['opciones'];
     if ($opciones=="edad"){
-
+        echo "<h2>Edad</h2>";
+        echo "Este apartado tendrá su respectivo contenido en un futuro. Perdón por las molestias.";
+    } elseif ($opciones=="nacionalidad"){
+        echo "<h2>Nacionalidad</h2>";
+        echo "Este apartado tendrá su respectivo contenido en un futuro. Perdón por las molestias.";
+    } elseif ($opciones=="relaciones"){
+        echo "<h2>Relaciones Excluidas y Especiales</h2>";
+        echo "Este apartado tendrá su respectivo contenido en un futuro. Perdón por las molestias.";
+    } else {
+        echo "<h1>Error en la selección.</h1>";
     }
 }
 ?>
