67baa4ed3203fefad90eeea1f6c158ee16a971ab
Pablo Úbeda <pubedac@campusdigitalfp.com>
2026-03-26 10:00:47 +0100

    footer `phph

--- barrasNavegacion/footer.php
+++ barrasNavegacion/footer.php
@@ -11,7 +11,7 @@
         </address> <!-- Cierre del address direccion-telefono -->
 
         <figure class="map-container"> <!-- Inicio del figure map-container (Extraído de Google Maps) -->
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d372.61746772248546!2d-0.8919604840284557!3d41.657043931848975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5914eaa1fa8ccd%3A0x64aae6a11ea538f3!2sM%C3%A9dicos%20del%20Mundo%20Zaragoza%2C%20Arag%C3%B3n!5e0!3m2!1ses!2ses!4v1771319093127!5m2!1ses!2ses" width="600" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d372.61746772248546!2d-0.8919604840284557!3d41.657043931848975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5914eaa1fa8ccd%3A0x64aae6a11ea538f3!2sM%C3%A9dicos%20del%20Mundo%20Zaragoza%2C%20Arag%C3%B3n!5e0!3m2!1ses!2ses!4v1771319093127!5m2!1ses!2ses" width="350" height="150" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             <!-- iFrame para cargar los datos de la sede de la ONG -->
         </figure> <!-- Cierre del figure map-container -->
 
