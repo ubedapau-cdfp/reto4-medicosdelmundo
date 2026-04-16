04740cd813d26ee4c1991470df2d0e6b52c2852f
garosv-tech <garosv@campusdigitalfp.com>
2026-04-16 11:32:29 +0200

    cambiado version php y cambiado div x section (gracias marta)

--- Vistadmin/Menu.php
+++ Vistadmin/Menu.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['usuario_id']) || !isset($_SESSION['id_rol']) || intval($_S
 		<a href="<?= $base ?>home/home.php" class="logo">
 			<img src="<?= $base ?>Imagenes/Logoreal.png" alt="Logo">
 		</a>
-		<div class="admin-session">
+		<section class="admin-session">
 			<?php
 			if (isset($_SESSION['usuario_nombre'])) {
 				$nombre = basename($_SESSION['usuario_nombre']);
@@ -29,7 +29,7 @@ if (!isset($_SESSION['usuario_id']) || !isset($_SESSION['id_rol']) || intval($_S
 				echo "<a class='logout-button' href='" . $base . "logout.php'>Cerrar sesión</a>";
 			}
 			?>
-		</div>
+		</section>
 	</header>
 	<main class="menu-grid">
 		<a class="menu-block block-contratos" href="<?= $base ?>contratos/relacionlaboral.php">
